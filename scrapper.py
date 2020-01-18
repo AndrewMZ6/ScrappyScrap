28d5e62d3ae3ddc0dcd405cc185556c3f6ad0c82
AndrewMZ <40640833+AndrewMZ6@users.noreply.github.com>
2020-01-18 14:11:32 +0800

    shows title and description of mods on nexus mods

---
+++ scrapper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.nexusmods.com/skyrimspecialedition'
+
+page_html = requests.get(URL)     
+
+page_soup = BeautifulSoup(page_html.content, 'html.parser')
+
+mods_container = page_soup.find(id = 'mod-list')  # len(mods_container) = 5  -----  contains <div id = 'mod-list'> that table with all the mods on the page
+
+mod_containers = mods_container.find_all('li', class_ = 'mod-tile') # len(mod_containers) = 20  ------ 20 mods shown on the page
+
+i = 0
+
+for k in mod_containers:  # for each of 20 mods do
+
+	container_title = mod_containers[i].find('div', class_ = 'tile-content') 
+	container_desc = container_title.p
+	container_title = container_title.h3
+	
+	container_title = container_title.text.strip()
+	container_desc = container_desc.text.strip()
+
+	i += 1
+
+	print('--------------================= iteration ' + str(i) + '=================----------------' + '\n')
+	print('TITLE = ',container_title + '\n' + 'DESCRIPTION = ' + container_desc + '\n')
